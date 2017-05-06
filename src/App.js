c500a60bdade4d7a1477f253e0a81b52c16f0fb1
Frederikam <frogkr@gmail.com>
2017-05-06 15:18:20 +0200

    Created header + ToC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import "./css/App.css";
+import logo from "../public/logo.svg";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <p>Hello, world!</p>
+                <div className="header">
+                    <img className="logo" src={logo} alt="Logo"/>
+                    <div className="logo-text">FredBoat</div>
+                </div>
+
+                <div className="table-of-contents">
+                    <div className="toc-title">
+                        Contents
+                    </div>
+                </div>
             </div>
         )
     }
