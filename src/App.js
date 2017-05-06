03b93dd18f538777ea8386b692bee0ae9016b665
Frederikam <frogkr@gmail.com>
2017-05-06 13:53:01 +0200

    Basic project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./css/App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello, world!</p>
+            </div>
+        )
+    }
+}
+
+export default App;
