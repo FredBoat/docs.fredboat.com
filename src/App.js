2bc874187b3558ce026b6f4818a48f5ea0dbd7fb
Frederikam <frogkr@gmail.com>
2017-05-07 15:56:43 +0200

    Split toc into a new file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "./css/App.css";
 import logo from "../public/logo.svg";
 import Markdown from "./Markdown";
-import TocHeader from "./TocHeader";
+import Toc from "./Toc";
 
 class App extends Component {
     render() {
@@ -14,20 +14,7 @@ class App extends Component {
                 </div>
 
                 <div className="page-body">
-                    <div className="table-of-contents">
-                        <div className="toc-title">
-                            Contents
-                        </div>
-                        <TocHeader activePage={this.props.page} page="index" name="Quickstart"/>
-                        <TocHeader activePage={this.props.page} page="faq" name="FAQ"/>
-                        <TocHeader activePage={this.props.page} page="terms" name="Terms of Service"/>
-                        
-                        <div className="toc-separator"/>
-                        <h2>Advanced</h2>
-
-                        <TocHeader activePage={this.props.page} page="selfhosting" name="Selfhosting"/>
-                        <TocHeader activePage={this.props.page} page="systemdservice" name="Selfhosting (systemd)"/>
-                    </div>
+                    <Toc/>
 
                     <Markdown name={this.props.page}/>
                 </div>
---
+++ src/Toc.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import "./css/Toc.css";
+import TocHeader from "./TocHeader";
+
+class Toc extends Component {
+    render() {
+        return (
+            <div className="Toc">
+                <div className="toc-title">
+                    Contents
+                </div>
+                <TocHeader activePage={this.props.page} page="index" name="Quickstart"/>
+                <TocHeader activePage={this.props.page} page="faq" name="FAQ"/>
+                <TocHeader activePage={this.props.page} page="terms" name="Terms of Service"/>
+
+                <div className="toc-separator"/>
+                <h2>Advanced</h2>
+
+                <TocHeader activePage={this.props.page} page="selfhosting" name="Selfhosting"/>
+                <TocHeader activePage={this.props.page} page="systemdservice" name="Selfhosting (systemd)"/>
+            </div>
+        )
+    }
+}
+
+export default Toc;
