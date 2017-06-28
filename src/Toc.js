f858bb5ad73aeb7dca42b9c14764a6ecabb101f8
Frederikam <frogkr@gmail.com>
2017-06-28 17:02:24 +0200

    Added carbon ads

--- src/Toc.js
+++ src/Toc.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import "./css/Toc.css";
 import TocHeader from "./TocHeader";
-import {Link} from 'react-router-dom'
 
 class Toc extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isAdblock: false,
-            showsAd: false
-        }
-    }
-
+    /*
     checkAdblock(instance) {
+
+         // Carbon is whitelisted
         // http://stackoverflow.com/questions/4869154/how-to-detect-adblock-on-my-website#20505898
 
         let adBlockEnabled = false;
@@ -33,35 +26,10 @@ class Toc extends Component {
                 showsAd: prevState.showsAd
             }));
         }, 100);
-    }
+
+    }*/
 
     render() {
-        let ad = null;
-        if(this.state.isAdblock) {
-            ad = (
-                <div id="ad-sidebar">
-                    <div className="adblock-notif">
-                        <p>I block ads too.</p>
-                        <p><Link to="/donate">Donate instead?</Link></p>
-                    </div>
-                </div>
-            );
-        } else {
-            ad = (
-                <div id="ad-sidebar">
-                    <ins className="adsbygoogle"
-                         style={{display: "inline-block", width: "336px", height: "280px"}}
-                         data-ad-client="ca-pub-6361371060709028"
-                         data-ad-slot="7243035790"
-                         data-ad-format="auto"/>
-                    <p className="about-ads">Ads keep FredBoat afloat. <Link to="/donate">Learn more</Link>.</p>
-                </div>
-            );
-
-            if(!this.state.showsAd)
-                this.checkAdblock(this);
-        }
-
         return (
             <div className="Toc">
                 <div className="toc-inner">
@@ -82,45 +50,22 @@ class Toc extends Component {
                     <TocHeader activePage={this.props.page} page="selfhosting" name="Selfhosting"/>
                     <TocHeader activePage={this.props.page} page="systemdservice" name="Selfhosting (systemd)"/>
                 </div>
-                {ad}
+
+                <div id="ad-sidebar"/>
             </div>
         )
     }
 
     //noinspection JSMethodCanBeStatic
-    componentDidUpdate() {
-        if(!this.state.isAdblock && !this.state.showsAd) {
-            console.log("Spawned ad");
-            const script1 = document.createElement("script");
+    componentDidMount() {
+        console.log("Spawned ad");
+        const script = document.createElement("script");
+        script.async = true;
+        script.type = "text/javascript";
+        script.src = "//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=fredboatcom";
+        script.id="_carbonads_js";
+        document.getElementById("ad-sidebar").appendChild(script);
 
-            script1.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-            script1.async = true;
-
-            document.getElementById("ad-sidebar").appendChild(script1);
-
-            const script2 = document.createElement("script");
-
-            script2.innerHTML =
-                `(adsbygoogle = window.adsbygoogle || []).push({});`;
-
-            document.getElementById("ad-sidebar").appendChild(script2);
-
-            this.setState((prevState) =>({
-                isAdblock: prevState.isAdblock,
-                showsAd: true
-            }));
-        } else if(this.state.isAdblock) {
-            window.setTimeout(function () {
-                if (document.getElementById("ad-sidebar") !== null) {
-                    document.getElementById("ad-sidebar").hidden = undefined;
-                    document.getElementById("ad-sidebar").id = "ad-sidebar-blocked"
-                }
-
-                if (document.getElementById("ad-sidebar-blocked") !== null) {
-                    document.getElementById("ad-sidebar-blocked").hidden = undefined;
-                }
-            }, 100);
-        }
     }
 }
 
